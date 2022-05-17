a191316bbf02ca644acfd42551d19735a72f4bf0
Alexandre Chau <accounts@chau.moe>
2022-05-17 16:32:25 +0200

    feat: add H5P upload function to public storage

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,52 @@
-export const TMP_EXTRACT_DIR = "./tmp/h5p/"
+export const TMP_EXTRACT_DIR = './tmp/h5p/';
+export const REMOTE_ROOT_H5P_STORAGE_DIR = '/h5p';
 
 export const MAX_FILE_SIZE = 1024 * 1024 * 50; // 50 MB
 export const MAX_NON_FILE_FIELDS = 0;
 export const MAX_FILES = 1;
 
 export const H5P_FILE_MIME_TYPE = 'application/zip';
+export const H5P_ALLOWED_FILE_EXTENSIONS = [
+  'bmp',
+  'css',
+  'csv',
+  'diff',
+  'doc',
+  'docx',
+  'eof',
+  'gif',
+  'jpeg',
+  'jpg',
+  'js',
+  'json',
+  'mp3',
+  'mp4',
+  'm4a',
+  'odp',
+  'ods',
+  'odt',
+  'ogg',
+  'otf',
+  'patch',
+  'pdf',
+  'png',
+  'ppt',
+  'pptx',
+  'rtf',
+  'svg',
+  'swf',
+  'textile',
+  'tif',
+  'tiff',
+  'ttf',
+  'txt',
+  'wav',
+  'webm',
+  'woff',
+  'xls',
+  'xlsx',
+  'xml',
+  'md',
+  'textile',
+  'vtt',
+];
--- src/service-api.ts
+++ src/service-api.ts
@@ -1,53 +1,36 @@
 import extract from 'extract-zip';
 import fs, { PathLike } from 'fs';
 import { lstat, mkdir, readdir, rm } from 'fs/promises';
+import mmm from 'mmmagic';
 import path from 'path';
 import { pipeline } from 'stream/promises';
+import util from 'util';
 import uuid from 'uuid';
 
 import fastifyMultipart from '@fastify/multipart';
 import { FastifyPluginAsync, FastifyRequest as Request } from 'fastify';
 
-import { Item } from 'graasp';
+import { Actor, Item } from 'graasp';
 import {
   FileTaskManager,
   GraaspLocalFileItemOptions,
   GraaspS3FileItemOptions,
   ServiceMethod,
 } from 'graasp-plugin-file';
-import { buildFilePathFromPrefix } from 'graasp-plugin-file-item';
 
 import {
+  H5P_ALLOWED_FILE_EXTENSIONS,
   H5P_FILE_MIME_TYPE,
   MAX_FILES,
   MAX_FILE_SIZE,
   MAX_NON_FILE_FIELDS,
+  REMOTE_ROOT_H5P_STORAGE_DIR,
   TMP_EXTRACT_DIR,
 } from './constants';
 import { InvalidH5PFileError } from './errors';
 
-/**
- * Validates H5P package content against the (poorly documented) H5P spec
- * https://h5p.org/documentation/developers/h5p-specification
- */
-async function validateH5P(
-  h5pFile: PathLike,
-  extractedContentDir: PathLike,
-): Promise<{ isValid: boolean; error?: string }> {}
-
-/**
- * Uploads both the .h5p and the package content into public storage
- */
-async function uploadH5P(
-  h5pFile: PathLike,
-  extractedContentDir: PathLike,
-): Promise<Partial<Item>> {
-}
-
-/**
- * Creates a Graasp item for the uploaded H5P package
- */
-async function createH5PItem(): Promise<Item> {}
+const magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE); // don't set MAGIC_CONTINUE!
+const detectMimeType = util.promisify(magic.detectFile.bind(magic));
 
 export interface H5PPluginOptions {
   pathPrefix: string;
@@ -66,6 +49,72 @@ const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) =>
 
   const fileTaskManager = new FileTaskManager(serviceOptions, serviceMethod);
 
+  /**
+   * Validates H5P package content against the (poorly documented) H5P spec
+   * https://h5p.org/documentation/developers/h5p-specification
+   */
+  async function validateH5P(
+    h5pFile: PathLike,
+    extractedContentDir: PathLike,
+  ): Promise<{ isValid: boolean; error?: string }> {}
+
+  /**
+   * Uploads both the .h5p and the package content into public storage
+   * Recursive function to traverse and upload the H5P folder
+   * IMPORTANT: the top-down traversal must not block or await (ensures that files
+   * are uploaded in parallel as soon as possible). Results aggregation can however
+   * await in parallel (note: await in a map fn does not block the map iteration).
+   */
+  async function uploadH5P(
+    folder: string,
+    uploadPath: string,
+    member: Actor,
+  ): Promise<Array<unknown>> {
+    const children = await readdir(folder);
+
+    return children.flatMap(async (child) => {
+      const childPath = path.join(folder, child);
+      const childUploadPath = path.join(uploadPath, child);
+      const stats = await lstat(child);
+
+      if (stats.isDirectory()) {
+        // recursively upload child folder
+        return await uploadH5P(childPath, childUploadPath, member);
+      } else {
+        // check if file extension is allowed
+        const extension = path.extname(childPath);
+        const normalizedExtension = (
+          extension[0] === '.' ? extension.slice(1) : extension
+        ).toLowerCase();
+        // ignore this file if extension is not allowed
+        if (!H5P_ALLOWED_FILE_EXTENSIONS.includes(normalizedExtension)) {
+          // we're using flatMap, represent none value with empty array
+          return [];
+        }
+
+        // SAFE CAST
+        // we are guaranteed that the resulting `mimetype` is a string
+        // from the docs: "Result is a string, except when MAGIC_CONTINUE is set"
+        const mimetype = (await detectMimeType(childPath)) as string;
+        const size = stats.size;
+        const uploadTask = fileTaskManager.createUploadFileTask(member, {
+          file: fs.createReadStream(childPath),
+          filepath: childUploadPath,
+          mimetype,
+          size,
+        });
+
+        // we're using flatMap, wrap result in array
+        return [await taskRunner.runSingle(uploadTask)];
+      }
+    });
+  }
+
+  /**
+   * Creates a Graasp item for the uploaded H5P package
+   */
+  async function createH5PItem(): Promise<Item> {}
+
   fastify.register(fastifyMultipart, {
     limits: {
       fields: MAX_NON_FILE_FIELDS,
@@ -83,12 +132,12 @@ const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) =>
       throw new InvalidH5PFileError(h5pFile.mimetype);
     }
 
-    const tmpId = uuid.v4();
-    const targetFolder = path.join(__dirname, TMP_EXTRACT_DIR, tmpId);
+    const contentId = uuid.v4();
+    const targetFolder = path.join(__dirname, TMP_EXTRACT_DIR, contentId);
     await mkdir(targetFolder, { recursive: true });
 
     try {
-      const savePath = path.join(targetFolder, `${tmpId}.h5p`);
+      const savePath = path.join(targetFolder, `${contentId}.h5p`);
       const contentFolder = path.join(targetFolder, 'content');
       // save H5P file
       await pipeline(h5pFile.file, fs.createWriteStream(savePath));
@@ -99,8 +148,9 @@ const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) =>
         throw new InvalidH5PFileError(error);
       }
 
-      // upload to public storage
-      await uploadH5P(savePath, contentFolder);
+      // upload whole folder to public storage
+      const remoteRootPath = path.join(REMOTE_ROOT_H5P_STORAGE_DIR, contentId);
+      await uploadH5P(targetFolder, remoteRootPath, member);
 
       const item = await createH5PItem();
 
