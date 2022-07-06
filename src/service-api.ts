1656beffb16e5ae51e5d41bd0011902a6a4eb2f6
Alexandre Chau <accounts@chau.moe>
2022-07-06 15:00:08 +0200

    refactor: fix store consistent file paths

--- src/service-api.ts
+++ src/service-api.ts
@@ -26,7 +26,7 @@ import { InvalidH5PFileError } from './errors';
 import { h5pImport } from './schemas';
 import { Service } from './service';
 import { H5PExtra, H5PPluginOptions, PermissionLevel } from './types';
-import { buildContentPath, buildH5PPath, buildRootPath } from './utils';
+import { buildContentPath, buildH5PPath, buildRootPath, buildRootRoute } from './utils';
 import { H5PValidator } from './validation/h5p-validator';
 
 const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) => {
@@ -103,6 +103,20 @@ const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) =>
   }
 
   /**
+   * Helper to create H5P extra
+   */
+  function buildH5PExtra(contentId: string): H5PExtra {
+    const rootRoute = buildRootRoute(pathPrefix, contentId);
+    return {
+      h5p: {
+        contentId,
+        h5pFilePath: buildH5PPath(rootRoute, contentId),
+        contentFilePath: buildContentPath(rootRoute),
+      },
+    };
+  }
+
+  /**
    * Creates a Graasp item for the uploaded H5P package
    * @param filename Name of the original H5P file
    * @param contentId Storage ID of the remote content
@@ -119,13 +133,7 @@ const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) =>
     const metadata = {
       name: filename.substring(0, ORIGINAL_FILENAME_TRUNCATE_LIMIT),
       type: H5P_ITEM_TYPE,
-      extra: {
-        h5p: {
-          contentId,
-          h5pFilePath: buildH5PPath(contentId), // <contentId>/<contentId>.h5p
-          contentFilePath: buildContentPath(contentId), // <contentId>/content
-        },
-      },
+      extra: buildH5PExtra(contentId),
     };
     const create = itemTaskManager.createCreateTaskSequence(member, metadata, parentId);
     return taskRunner.runSingleSequence(create) as Promise<Item<H5PExtra>>;
@@ -263,11 +271,7 @@ const plugin: FastifyPluginAsync<H5PPluginOptions> = async (fastify, options) =>
       });
       await taskRunner.runSingle(copyTask);
 
-      item.extra.h5p = {
-        contentId,
-        h5pFilePath: buildH5PPath(remoteRootPath, contentId),
-        contentFilePath: buildContentPath(remoteRootPath),
-      };
+      item.extra.h5p = buildH5PExtra(contentId).h5p;
     });
   });
 };
--- src/utils.ts
+++ src/utils.ts
@@ -3,15 +3,29 @@ import path from 'path';
 import { H5P } from './validation/h5p';
 
 /**
- * Helper to build the root remote path
+ * Helper to build the root remote path for a specific H5P
+ * @note
+ * We want the following path structure: pathPrefix/pathPrefix/contentId
+ * This allows serving pathPrefix as root URL, while keeping the routes
+ * matched to the physical paths of the files
+ * @see buildRootRoute
  */
 export const buildRootPath = (pathPrefix: string, contentId: string) =>
+  path.join(pathPrefix, pathPrefix, contentId);
+
+/**
+ * Helper to build the route for a specific H5P
+ * @note
+ * We want the following route structure: pathPrefix/contentId
+ * @see buildRootPath
+ */
+export const buildRootRoute = (pathPrefix: string, contentId: string) =>
   path.join(pathPrefix, contentId);
 
 /**
  * Helper to build the local or remote path of the .h5p file
  */
-export const buildH5PPath = (rootPath: string, contentId: string = rootPath) =>
+export const buildH5PPath = (rootPath: string, contentId: string) =>
   path.join(rootPath, `${contentId}.${H5P.H5P_FILE_EXTENSION}`);
 
 /**
